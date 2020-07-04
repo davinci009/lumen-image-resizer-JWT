f7313ecd584c1884974d7b13d263fd86a23c3926
DavinciCode <solucionesborgesinformatico@gmail.com>
2020-07-04 14:13:06 -0400

    user migration added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UserController extends Controller
+{
+    
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        
+    }
+}
